ad6b9d126fc900acfd49f079c6ba20b017ea3c2e
Scott Hyndman <scotty.hyndman@gmail.com>
2023-12-08 07:41:15 -0500

    Add a little utility function for timing function invocations

---
+++ src/debug.rs
@@ -0,0 +1,11 @@
+extern crate alloc;
+
+use defmt::println;
+use embassy_time::Instant;
+
+pub fn time<BLOCK: FnOnce<()>>(label: &'static str, block: BLOCK) -> BLOCK::Output {
+    let ts = Instant::now();
+    let ret = block();
+    println!("{} in {}us", label, (Instant::now() - ts).as_micros());
+    ret
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(tuple_trait, unboxed_closures)]
 
 pub mod parser;
 pub mod reciter;
 pub mod renderer;
+
+mod debug;
