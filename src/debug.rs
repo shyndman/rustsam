46be7e6abf5908acab5fdad40eb94a5460be181e
Scott Hyndman <scotty.hyndman@gmail.com>
2023-12-08 10:04:44 -0500

    Permit debugging code to be unused without warnings

--- src/debug.rs
+++ src/debug.rs
@@ -3,6 +3,7 @@ extern crate alloc;
 use defmt::println;
 use embassy_time::Instant;
 
+#[allow(dead_code)]
 pub fn time<BLOCK: FnOnce<()>>(label: &'static str, block: BLOCK) -> BLOCK::Output {
     let ts = Instant::now();
     let ret = block();
